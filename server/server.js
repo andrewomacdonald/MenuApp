69d86eb8c91eb28e652428f084fd5ad66508086a
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-03-18 16:58:35 -0700

    [Feature] Removed test functions from server.js, added userprefs table, tested relationships between tables

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ var express = require('express');
 var request = require('request');
 
 var app = express();
-var user = require('./db/models/User.js');
-var item = require('./db/models/Item_Rating.js');
-var restaurant = require('./db/models/Restaurant.js');
-var menu = require('./db/models/Menu_Item.js');
 var bodyParser = require('body-parser');
+var userPreference = require('./db/models/User_Preference.js');
 var db = require('./db/schema.js');
 var jwt = require('jwt-simple');
 app.use(bodyParser.json());
@@ -18,64 +15,5 @@ var server = app.listen(port, function() {
   console.log('http://localhost:' + port);
 });
 
-var test = function(){
-  var rating = 5;
-  new item({rating: rating})
-    .fetch()
-    .then(function(itemRating){
-      if(!itemRating) {
-        var newRating = new item({
-          rating: rating
-        });
-      newRating.save()
-        .then(function (){
-          console.log('Rating Inserted');
-        })
-      } else {
-        console.log('Rating not inserted');
-      }
-    })
-}
-
-var test1 = function(){
-  var entree = "salisbury steak";
-  new menu({item: entree})
-    .fetch()
-    .then(function(dish) {
-      if(!dish){
-        var newDish = new menu({
-          item: entree
-        });
-      newDish.save()
-        .then(function () {
-          console.log('entree inserted');
-        })
-      } else {
-        console.log('entree not inserted');
-      }
-    })
-}
-
-var test2 = function(){
-  var eatery = "denny's";
-  new restaurant({restaurant_id: eatery})
-    .fetch()
-    .then(function(place) {
-      if(!place){
-        var newRestaurant = new restaurant({
-          restaurant_id: eatery
-        });
-    newRestaurant.save()
-      .then(function() {
-        console.log('restaurant is inserted');
-      })
-    } else {
-      console.log('restaurant not inserted');
-    }
-  })
-}
-test();
-test1();
-test2();
 
 module.exports = app;
